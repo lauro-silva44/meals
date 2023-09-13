a4c45ede5ad9e185f186bf5d97db757d789cc87f
lauro <lauro@in8.com.br>
2023-09-14 00:01:11 +0100

    inserted detail screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/models/category.dart';
+import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _onSelectCategory(BuildContext context, Category category) {
+    final filterMeal = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+    Navigator.of(context).push(MaterialPageRoute(
+        builder: (ctx) =>
+            MealsScreen(title: category.title, meals: filterMeal)));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +30,12 @@ class CategoriesScreen extends StatelessWidget {
             mainAxisSpacing: 20,
             crossAxisSpacing: 20),
         children: availableCategories
-            .map((category) => CategoryGridItem(category: category))
+            .map((category) => CategoryGridItem(
+                  category: category,
+                  onSelectedCategory: () {
+                    _onSelectCategory(context, category);
+                  },
+                ))
             .toList(),
       ),
     );
